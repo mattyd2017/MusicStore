3ee1854d0f71a7c5d5c7a57f395e0fd2709e28d6
mattyd2017 <32998214+mattyd2017@users.noreply.github.com>
2018-07-27 11:20:41 +0100

    music5

--- MusicStore/Areas/Admin/Controllers/ShopController.cs
+++ MusicStore/Areas/Admin/Controllers/ShopController.cs
@@ -2,8 +2,10 @@
 using MusicStore.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace MusicStore.Areas.Admin.Controllers
@@ -131,6 +133,7 @@ namespace MusicStore.Areas.Admin.Controllers
             return "ok";
         }
         //GET: admin/shop/AddProduct
+        [HttpGet]
         public ActionResult AddProduct()
         {
             //init model
@@ -145,6 +148,144 @@ namespace MusicStore.Areas.Admin.Controllers
 
                 //return view with model
                 return View(model);
+        }
+        //POST: admin/shop/AddProduct
+        [HttpPost]
+        public ActionResult AddProduct(ProductVm model, HttpPostedFileBase file)
+        {
+            // check model state
+            if(!ModelState.IsValid)
+            {
+                using (Db db = new Db())
+                {
+                    model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                    return View(model);
+                }
+
+            }
+
+            //make sure product name is unique
+            using (Db db = new Db())
+            {
+                if (db.Products.Any(x => x.Name == model.Name))
+                {
+                    model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                    ModelState.AddModelError("", "That product name already exists please use another");
+                    return View(model);
+                }
+            }
+
+            //declare product id
+            int id;
+
+            //init and save productDTO
+            using (Db db = new Db())
+            {
+                ProductDTO product = new ProductDTO();
+
+                product.Name = model.Name;
+                product.Slug = model.Name.Replace(" ", "-").ToLower();
+                product.Description = model.Description;
+                product.Price = model.Price;
+                product.CategoryId = model.CategoryId;
+
+                CategoryDTO catDTO = db.Categories.FirstOrDefault(x => x.Id == model.CategoryId);
+                product.CategoryName = catDTO.Name;
+
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                // get the id
+                id = product.Id;
+            }
+
+            //set tempdate message
+            TempData["SM"] = "You have successfully added a product!!";
+
+
+
+            #region Upload Image
+
+            //create directories for images
+            var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+
+
+            var pathString1 = Path.Combine(originalDirectory.ToString(), "Products");
+            var pathString2 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString());
+            var pathString3 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Thumbs");
+            var pathString4 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery");
+            var pathString5 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery\\Thumbs");
+
+            if (!Directory.Exists(pathString1))
+                Directory.CreateDirectory(pathString1);
+
+            if (!Directory.Exists(pathString2))
+                Directory.CreateDirectory(pathString2);
+
+            if (!Directory.Exists(pathString3))
+                Directory.CreateDirectory(pathString3);
+
+            if (!Directory.Exists(pathString4))
+                Directory.CreateDirectory(pathString4);
+
+            if (!Directory.Exists(pathString5))
+                Directory.CreateDirectory(pathString5);
+
+            //check if file was uploaded
+
+            if (file != null && file.ContentLength > 0)
+            {
+                //get file extension
+                string ext = file.ContentType.ToLower();
+                //verify file extension
+
+                if(ext != "image/jpg"&&
+                   ext != "image/jpeg"&&
+                   ext != "image/pjpeg"&&
+                   ext != "image/gif"&&
+                   ext != "image/png"&&
+                   ext != "image/x-png")
+                {
+                    using (Db db = new Db())
+                    {
+
+                          model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                          ModelState.AddModelError("", "The image was not uploaded to server - wrong image format");
+                          return View(model);
+
+                    }
+
+                }
+
+                //init file name
+                string imageName = file.FileName;
+
+                //save image name to dto
+                using (Db db = new Db())
+                {
+                    ProductDTO dto = db.Products.Find(id);
+                    dto.ImageName = imageName;
+
+                    db.SaveChanges();
+                }
+
+                //set original and thumb image paths
+                var path = string.Format("{0}\\{1}", pathString2, imageName);
+                var path2 = string.Format("{0}\\{1}", pathString3, imageName);
+
+                //save original image
+                file.SaveAs(path);
+
+                //create and save thumb image
+                WebImage img = new WebImage(file.InputStream);
+                img.Resize(300, 300);
+                img.Save(path2);
+            }
+            #endregion
+
+
+            //redirect
+            return RedirectToAction("AddProduct");
         }
     }
 }
